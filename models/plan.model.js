a2a18fcc0ef54d47febeaf3e0ab768c82aeded79
Shovon Das <shovondas26@gmail.com>
2020-10-25 11:51:30 +0600

    plan  model created

---
+++ models/plan.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const PlanSchema = new Schema({
+    title: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    commision: {
+        type: Number,
+    },
+    duration: {
+        type: String,
+    },
+    feature: {
+        type: Boolean,
+    },
+})
+
+const Plan = mongoose.model('Plans', PlanSchema)
+module.exports = Plan
