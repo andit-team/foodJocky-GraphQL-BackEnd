6aa8d4477a8d50abbb79436c10e2320c5a5ce909
AndiT Service <webguru071@gmail.com>
2020-11-02 14:56:09 +0600

    get all and one

--- controllers/food/food.controller.js
+++ controllers/food/food.controller.js
@@ -101,7 +101,6 @@ exports.deleteFood = async (root, args, context) => {
     // }
 };
 
-<<<<<<< HEAD
 exports.getAllFoods = async(root, args, context) => {
     
     try{
@@ -171,66 +170,3 @@ exports.getOneFood = async(root, args, context) => {
     
 
 }
-=======
-exports.getAllFoods = async (root, args, context) => {
-    // try{
-    //     const hash = bcrypt.hashSync(args.ownerInput.password, 8);
-    //     let newOwner = new User({
-    //         first_name: args.ownerInput.first_name,
-    //         last_name: args.ownerInput.last_name,
-    //         mobile: args.ownerInput.mobile,
-    //         email: args.ownerInput.email,
-    //         type: args.ownerInput.type,
-    //         owner_address: args.ownerInput.owner_address,
-    //         password: hash,
-    //         status: "pending",
-    //         national_id: args.ownerInput.national_id
-    //     })
-    //     let nOwner = await newOwner.save();
-    //     let returnData = {
-    //         error: false,
-    //         msg: "Owner Created Successfully",
-    //         data: nOwner
-    //     }
-    //     return returnData
-    // }catch(error){
-    //     let returnData = {
-    //         error: true,
-    //         msg: "Mobile Number Already Taken",
-    //         data: {}
-    //     }
-    //     return returnData
-    // }
-};
-
-exports.getOneFood = async (root, args, context) => {
-    // try{
-    //     const hash = bcrypt.hashSync(args.ownerInput.password, 8);
-    //     let newOwner = new User({
-    //         first_name: args.ownerInput.first_name,
-    //         last_name: args.ownerInput.last_name,
-    //         mobile: args.ownerInput.mobile,
-    //         email: args.ownerInput.email,
-    //         type: args.ownerInput.type,
-    //         owner_address: args.ownerInput.owner_address,
-    //         password: hash,
-    //         status: "pending",
-    //         national_id: args.ownerInput.national_id
-    //     })
-    //     let nOwner = await newOwner.save();
-    //     let returnData = {
-    //         error: false,
-    //         msg: "Owner Created Successfully",
-    //         data: nOwner
-    //     }
-    //     return returnData
-    // }catch(error){
-    //     let returnData = {
-    //         error: true,
-    //         msg: "Mobile Number Already Taken",
-    //         data: {}
-    //     }
-    //     return returnData
-    // }
-};
->>>>>>> shovon
