820e4f9d7f1ee60edb73970717b361873bce3075
AndiT Service <webguru071@gmail.com>
2020-11-02 12:54:42 +0600

    getall get one init

--- controllers/food/food.controller.js
+++ controllers/food/food.controller.js
@@ -122,80 +122,70 @@ exports.deleteFood = async(root, args, context) => {
 
 exports.getAllFoods = async(root, args, context) => {
     
-    // try{
+    try{
 
-    //     const hash = bcrypt.hashSync(args.ownerInput.password, 8);
-    //     let newOwner = new User({
-    //         first_name: args.ownerInput.first_name,
-    //         last_name: args.ownerInput.last_name,
-    //         mobile: args.ownerInput.mobile,
-    //         email: args.ownerInput.email,
-    //         type: args.ownerInput.type,
-    //         owner_address: args.ownerInput.owner_address,
-    //         password: hash,
-    //         status: "pending",
-    //         national_id: args.ownerInput.national_id
-    //     })
+        let foods = await Restaurant.find(
+            {
+                _id: args.foodParams.restaurant_id,
+                'food_categories._id': args.foodParams.food_categories_id
+            },
+            {
+                'food_categories.$.foods.$': 1 
+            }
+            )
 
-    //     let nOwner = await newOwner.save();
+        let returnData = {
+            error: false,
+            msg: "Food Get Successfully",
+            data: foods
+        }
+        return returnData
 
-    //     let returnData = {
-    //         error: false,
-    //         msg: "Owner Created Successfully",
-    //         data: nOwner
-    //     }
-    //     return returnData
+    }catch(error){
 
-    // }catch(error){
+        let returnData = {
+            error: true,
+            msg: "Food Get UnSuccessfully",
+            data: {}
+        }
+        return returnData
 
-    //     let returnData = {
-    //         error: true,
-    //         msg: "Mobile Number Already Taken",
-    //         data: {}
-    //     }
-    //     return returnData
-
-    // }
+    }
     
 
 }
 
 exports.getOneFood = async(root, args, context) => {
     
-    // try{
+    try{
 
-    //     const hash = bcrypt.hashSync(args.ownerInput.password, 8);
-    //     let newOwner = new User({
-    //         first_name: args.ownerInput.first_name,
-    //         last_name: args.ownerInput.last_name,
-    //         mobile: args.ownerInput.mobile,
-    //         email: args.ownerInput.email,
-    //         type: args.ownerInput.type,
-    //         owner_address: args.ownerInput.owner_address,
-    //         password: hash,
-    //         status: "pending",
-    //         national_id: args.ownerInput.national_id
-    //     })
+        let foods = await Restaurant.find(
+            {
+                _id: args.foodParams.restaurant_id,
+                'food_categories._id': args.foodParams.food_categories_id
+            },
+            {
+                'food_categories.$.foods.$': 1 
+            }
+            )
 
-    //     let nOwner = await newOwner.save();
+        let returnData = {
+            error: false,
+            msg: "Food Get Successfully",
+            data: foods
+        }
+        return returnData
 
-    //     let returnData = {
-    //         error: false,
-    //         msg: "Owner Created Successfully",
-    //         data: nOwner
-    //     }
-    //     return returnData
+    }catch(error){
 
-    // }catch(error){
+        let returnData = {
+            error: true,
+            msg: "Food Get UnSuccessfully",
+            data: {}
+        }
+        return returnData
 
-    //     let returnData = {
-    //         error: true,
-    //         msg: "Mobile Number Already Taken",
-    //         data: {}
-    //     }
-    //     return returnData
-
-    // }
+    }
     
 
 }
